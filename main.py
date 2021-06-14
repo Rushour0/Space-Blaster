bd54b12bbe5132056eb48a0fb669989ef9165dd8
Rushour0 <pataderushikesh@gmail.com>
2021-06-15 00:29:42 +0530

    main

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
+import sys
 import pygame
 
+sys.path.append("src")
+sys.path.append("img")
+from Blaster import *
+from paths import *
 # PyGame initialized
 pygame.init()
 
+WINDOW_DIMENSIONS = (800,600)
+WINDOW_WIDTH,WINDOW_HEIGHT = WINDOW_DIMENSIONS
 # Create screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode(WINDOW_DIMENSIONS)
 
 # Set window name
-pygame.display,set_caption("Space Blaster")
+pygame.display.set_caption("Space Blaster")
 
 # Load logo image and set icon
-icon = pygame.imgae.load("img/icon.png")
+icon = pygame.image.load(icon_img)
 pygame.display.set_icon(icon)
 
+# Blaster declaration
+spaceship = Blaster(spaceship_img,WINDOW_WIDTH/2,WINDOW_HEIGHT-WINDOW_HEIGHT/8)
 
+def show_me():
+	spaceship.changeXY(WINDOW_DIMENSIONS = WINDOW_DIMENSIONS)
+	screen.blit(*list(spaceship.load()))
 running = True
 
 def gameLoop():
+
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			return False
-	
 
+		if event.type == pygame.KEYDOWN:
+			print(pygame.key.get_pressed().index(1))
+			if event.key in [pygame.K_LEFT,pygame.K_a]:
+				spaceship.x_chg = -spaceship.default_x
+
+			if event.key in [pygame.K_RIGHT,pygame.K_d]:
+				spaceship.x_chg = spaceship.default_x
+
+		if event.type == pygame.KEYUP:
+			if event.key in [pygame.K_LEFT,pygame.K_a] and not (pygame.key.get_pressed()[79] or pygame.key.get_pressed()[7]):
+				spaceship.x_chg = 0
+
+			if event.key in [pygame.K_RIGHT,pygame.K_d] and not (pygame.key.get_pressed()[80] or pygame.key.get_pressed()[4]):
+				spaceship.x_chg = 0
+
+
+			
+	show_me()
+	pygame.display.update()
+	
+	return True
 while running:
-	if (gameLoop() == False):return
+	if (gameLoop() == False):break
--- src/Blaster.py
+++ src/Blaster.py
@@ -13,15 +13,17 @@ class Blaster:
 		self.y_chg = 0
 		self.default_x = 1
 		self.default_y = 1
+		self.xc = self.x-self.width/2
+		self.yc = self.y-self.height/2
 		print(self.width,self.height)
 
 	def load(self):
-		return self.img,(self.x,self.y)
+		return self.img,tuple([self.x-self.width/2,self.y-self.height/2])
 
-	def changeXY(self,x = 0,y = 0,WINDOW_WIDTH = None,WINDOW_HEIGHT = None,WINDOW_DIMENSIONS = None):
+	def changeXY(self,WINDOW_WIDTH = None,WINDOW_HEIGHT = None,WINDOW_DIMENSIONS = None):
 		if WINDOW_DIMENSIONS is not None:
 			WINDOW_WIDTH,WINDOW_HEIGHT = WINDOW_DIMENSIONS
-		if 0<self.x+self.x_chg and self.x+self.x_chg+self.width<WINDOW_WIDTH:
+		if self.width/2<self.x+self.x_chg and self.x+self.x_chg+self.width<WINDOW_WIDTH+self.width/2:
 			self.x+=self.x_chg
-		if 0<self.y+self.y_chg and self.y+self.y_chg+self.height<WINDOW_HEIGHT:
+		if self.width/2<self.y+self.y_chg and self.y+self.y_chg+self.height<WINDOW_HEIGHT+self.width/2:
 			self.y+=self.y_chg
