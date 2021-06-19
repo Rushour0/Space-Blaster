650b65bccde40b32a948f6b857b925c1a6a47318
Rushour0 <pataderushikesh@gmail.com>
2021-06-20 01:08:00 +0530

    main

--- src/Bullet.py
+++ src/Bullet.py
@@ -5,15 +5,15 @@ class DisplayBullet:
 	def __init__(self,img_name,x,y):
 		img = pygame.image.load(img_name)
 		self.img_path = img_name
+		self.x = x
+		self.y = y
 		self.img = img
 		self.width = img.get_width()
 		self.height = img.get_height()
-		self.x = x-self.width/2
-		self.y = y-self.height/2
 		# print(self.width,self.height)
 
 	def load(self):
-		return self.img,tuple([self.x,self.y])
+		return self.img,tuple([self.x-self.width/2,self.y-self.height/2])
 
 # Defining Laser class for firing bullets/lasers
 class Laser(DisplayBullet):
@@ -25,9 +25,9 @@ class Laser(DisplayBullet):
 	def changeXY(self,WINDOW_WIDTH = None,WINDOW_HEIGHT = None,WINDOW_DIMENSIONS = None):
 		if WINDOW_DIMENSIONS is not None:
 			WINDOW_WIDTH,WINDOW_HEIGHT = WINDOW_DIMENSIONS
-		if 0<self.x+self.x_chg and self.x+self.x_chg+self.width<WINDOW_WIDTH:
+		if self.width/2<self.x+self.x_chg and self.x+self.x_chg+self.width<WINDOW_WIDTH+self.width/2:
 			self.x+=self.x_chg
-		if 0<self.y+self.y_chg and self.y+self.y_chg+self.height<WINDOW_HEIGHT:
+		if self.width/2<self.y+self.y_chg and self.y+self.y_chg+self.height<WINDOW_HEIGHT+self.width/2:
 			self.y-=self.y_chg
 		else:
 			return False
