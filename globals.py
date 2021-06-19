8d0535c2d4519729242cfc94b46cb7afaf63aeb4
Rushour0 <pataderushikesh@gmail.com>
2021-06-20 02:45:00 +0530

    main

--- globals.py
+++ globals.py
@@ -8,19 +8,19 @@ sys.path.append("img")
 sys.path.append("music")
 
 # laser bullets limit
-bullet_limit = 2
+bullet_limit = 5
 
 # Time for generation of new asteroid
-time_elapse = 0.3
+time_elapse = 0.2
 
 # Window Dimensions
 WINDOW_DIMENSIONS = (800,600)
 WINDOW_WIDTH,WINDOW_HEIGHT = WINDOW_DIMENSIONS
 
 # Universal spawning coordinates for the asteroids
-uni_asteroid_spawn_x = [list([i for i in range(j,800,48)])[1:-1] for j in range(2,14)]
+uni_asteroid_spawn_x = [list([i for i in range(j,800,36)])[1:-1] for j in range(2,14)]
 
-uni_asteroid_spawn_y = 32
+uni_asteroid_spawn_y = 0
 # importing all classes and extra methods
 from Blaster import *
 from Bullet import *
--- src/Asteroid.py
+++ src/Asteroid.py
@@ -20,17 +20,24 @@ class Asteroid:
 		self.width = img.get_width()
 		self.height = img.get_height()
 		self.x_chg = 0
-		self.y_chg = 0.75
-
+		self.y_chg = 1.5
+		self.rotate_value = random.randrange(-7,7,3)
+		self.rotate_counter = 0
+		self.rotate_after = 110
 	def load(self):
 		return self.img,tuple([self.x-self.width/2,self.y-self.height/2])
 
 	def changeXY(self,WINDOW_WIDTH = None,WINDOW_HEIGHT = None,WINDOW_DIMENSIONS = None):
+	
+		if not self.rotate_counter%self.rotate_after:
+			self.img = pygame.transform.rotate(self.img,random.randrange(-1,1,2)*self.rotate_value+random.randint(-2,2))
+		self.rotate_counter+=1
+
 		if WINDOW_DIMENSIONS is not None:
 			WINDOW_WIDTH,WINDOW_HEIGHT = WINDOW_DIMENSIONS
 		if self.width/2<self.x+self.x_chg and self.x+self.x_chg+self.width<WINDOW_WIDTH+self.width*1.5:
 			self.x+=self.x_chg
-		if self.height/2<self.y+self.y_chg and self.y+self.y_chg+self.height<WINDOW_HEIGHT+self.height*1.5:
+		if -self.height<self.y+self.y_chg and self.y+self.y_chg+self.height<WINDOW_HEIGHT+self.height*1.5:
 			self.y+=self.y_chg
 		else:
 			return False
