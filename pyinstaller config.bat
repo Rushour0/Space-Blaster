d6a9891f61d87e6b0a9810bd7a3b780e77966940
Rushour0 <pataderushikesh@gmail.com>
2021-06-20 20:52:55 +0530

    main

--- pyinstaller config.bat
+++ pyinstaller config.bat
@@ -2,4 +2,5 @@ copy "D:\Projects\Space Blaster\fonts\" D:\Projects\Game\
 copy "D:\Projects\Space Blaster\music\" D:\Projects\Game\
 copy "D:\Projects\Space Blaster\img\" D:\Projects\Game\
 cd D:\Projects\Game\
+
 pyinstaller --noconfirm --onefile --windowed --icon "D:/Projects/Space Blaster/img/spaceship.ico" --name "Space Blaster" --paths "D:/Projects/Space Blaster/img" --paths "D:/Projects/Space Blaster/music" --paths "D:/Projects/Space Blaster/src" --paths "D:/Projects/Space Blaster/fonts" --add-data "D:/Projects/Space Blaster/fonts;fonts/" --add-data "D:/Projects/Space Blaster/img;img/" --add-data "D:/Projects/Space Blaster/music;music/"  "D:/Projects/Space Blaster/Space Blaster.py"
